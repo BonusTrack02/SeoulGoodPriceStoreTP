69ef17d850282ea21cdbb9e54685b3a2b1ed1962
BonusTrack02 <wlsgud321@gmail.com>
2022-03-22 16:41:42 +0900

    v1.2 Added SeoulFont Added Fragments 30 index per Fragment Edited BottomNavigationView color

---
+++ app/src/main/java/com/bonustrack02/tp08goodprice/KoreanFragment.java
@@ -0,0 +1,138 @@
+package com.bonustrack02.tp08goodprice;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class KoreanFragment extends Fragment {
+
+    ArrayList<Item> items = new ArrayList<>();
+    RecyclerView recyclerView;
+    RecyclerAdapter adapter;
+    ProgressBar progressBar;
+    String apiKey = "4e4c586954776c7338365056587546";
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_korean, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        progressBar = view.findViewById(R.id.progressbar);
+        adapter = new RecyclerAdapter(getActivity(), items);
+        recyclerView = view.findViewById(R.id.recycler);
+
+        loadApiData();
+
+        recyclerView.setAdapter(adapter);
+    }
+
+    void loadApiData() {
+        Thread thread = new Thread() {
+
+            @Override
+            public void run() {
+                int startIndex = 1, endIndex = 30;
+
+                String apiAddress = "http://openapi.seoul.go.kr:8088/"
+                        + apiKey + "/xml/ListPriceModelStoreService/"
+                        + startIndex+ "/" + endIndex + "/001/";
+                try {
+                    URL url = new URL(apiAddress);
+
+                    InputStream inputStream = url.openStream();
+                    InputStreamReader reader = new InputStreamReader(inputStream);
+
+                    XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+                    XmlPullParser parser = factory.newPullParser();
+                    parser.setInput(reader);
+
+                    int eventType = parser.getEventType();
+
+                    Item item = null;
+
+                    while (eventType != XmlPullParser.END_DOCUMENT) {
+                        switch (eventType) {
+                            case XmlPullParser.START_DOCUMENT:
+                                getActivity().runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        progressBar.setVisibility(View.VISIBLE);
+                                    }
+                                });
+                                break;
+
+                            case XmlPullParser.START_TAG:
+                                String startTag = parser.getName();
+                                if (startTag.equals("row")) {
+                                    item = new Item();
+                                    Log.i("Tag", "row");
+                                } else if (startTag.equals("SH_NAME")) {
+                                    parser.next();
+                                    if (item != null) item.name = parser.getText();
+                                } else if (startTag.equals("SH_ADDR")) {
+                                    parser.next();
+                                    if (item != null) item.address = parser.getText();
+                                } else if (startTag.equals("SH_PHONE")) {
+                                    parser.next();
+                                    if (item != null) item.phone = parser.getText();
+                                } else if (startTag.equals("SH_PHOTO")) {
+                                    parser.next();
+                                    if (item != null) item.imgShop = parser.getText();
+                                }
+                                break;
+
+                            case XmlPullParser.END_TAG:
+                                String endTag = parser.getName();
+                                if (endTag.equals("row")) if (item != null) items.add(item);
+                                break;
+
+                            case XmlPullParser.TEXT:
+                                break;
+                        }
+                        eventType = parser.next();
+                    }
+
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            if (adapter != null) adapter.notifyDataSetChanged();
+                            Log.i("Tag", "" + items.size());
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    });
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (XmlPullParserException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+    }
+
+}
--- app/src/main/java/com/bonustrack02/tp08goodprice/MainActivity.java
+++ app/src/main/java/com/bonustrack02/tp08goodprice/MainActivity.java
@@ -1,14 +1,22 @@
 package com.bonustrack02.tp08goodprice;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
@@ -27,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
     RecyclerAdapter adapter;
+    BottomNavigationView bottomNavigationView;
+    FragmentManager fragmentManager;
+    ArrayList<Fragment> fragments = new ArrayList<>();
 
     String apiKey = "4e4c586954776c7338365056587546";
 
@@ -35,21 +46,68 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recyclerView = findViewById(R.id.recycler);
-        adapter = new RecyclerAdapter(this, items);
-        recyclerView.setAdapter(adapter);
+        fragmentManager = getSupportFragmentManager();
 
-        loadApiData();
+        fragments.add(new KoreanFragment());
+
+        fragmentManager.beginTransaction().add(R.id.container, fragments.get(0)).commit();
+        for (int i = 0; i < 3; i++) {
+            fragments.add(null);
+        }
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+                for (int j = 0; j < 4; j++) {
+                    if (fragments.get(j) != null) transaction.hide(fragments.get(j));
+                }
+
+                switch (item.getItemId()) {
+                    case R.id.menu_bottom_korean:
+                        transaction.show(fragments.get(0));
+                        break;
+
+                    case R.id.menu_bottom_chinese:
+                        if (fragments.get(1) == null) {
+                            fragments.set(1, new ChineseFragment());
+                            transaction.add(R.id.container, fragments.get(1));
+                        }
+                        transaction.show(fragments.get(1));
+                        break;
+
+                    case R.id.menu_bottom_japanese:
+                        if (fragments.get(2) == null) {
+                            fragments.set(2, new JapaneseFragment());
+                            transaction.add(R.id.container, fragments.get(2));
+                        }
+                        transaction.show(fragments.get(2));
+                        break;
+
+                    case R.id.menu_bottom_others:
+                        if (fragments.get(3) == null) {
+                            fragments.set(3, new OthersFragment());
+                            transaction.add(R.id.container, fragments.get(3));
+                        }
+                        transaction.show(fragments.get(3));
+                        break;
+                }
+                transaction.commit();
+                return true;
+            }
+        });
     }
 
     void loadApiData() {
         Thread thread = new Thread() {
 
-            int startIndex = 1, endIndex = 50;
+            int startIndex = 1, endIndex = 50, dirCode = 001;
 
             String apiAddress = "http://openapi.seoul.go.kr:8088/"
                     + apiKey + "/xml/ListPriceModelStoreService/"
-                    + startIndex+ "/" + endIndex + "/001/";
+                    + startIndex+ "/" + endIndex + "/" + dirCode + "/";
 
             ProgressBar progressBar = findViewById(R.id.progressbar);
 
