fd8b3b40d2a2c88dc2c6733ef098314e62020f24
BonusTrack02 <wlsgud321@gmail.com>
2023-05-01 01:20:03 +0900

    오픈소스 라이선스 추가를 위해 툴바 추가

--- app/src/main/java/com/bonustrack02/tp08goodprice/MainActivity.kt
+++ app/src/main/java/com/bonustrack02/tp08goodprice/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.bonustrack02.tp08goodprice
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -18,6 +21,9 @@ class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
 
         fragments.add(KoreanFragment())
 
@@ -59,6 +65,22 @@ class MainActivity: AppCompatActivity() {
             transaction.commit()
             return@setOnItemSelectedListener true
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.option, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_about -> {
+                val intent = Intent(this, AboutActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
     var wasPressed = false
